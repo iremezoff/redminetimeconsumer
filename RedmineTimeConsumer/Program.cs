246efebd85ec9358a12333e936b40645ab9215a8
iremezoff <remezovis@ugsk.ru>
2015-02-16 09:31:21 +0500

    fixed over RM api bugs

--- RedmineTimeConsumer/Program.cs
+++ RedmineTimeConsumer/Program.cs
@@ -25,16 +25,39 @@ namespace RedmineTimeConsumer
             var employeeName = args[0];
             string date = args.Length < 2 ? DateTime.Now.ToString("yyyy-MM-dd") : args[1];
 
-
-
             string host = "http://redmine.ugsk.ru";
-            string apiKey = "cf312b768b25dcd8d6de335762f359d891544a28";
+            //string apiKey = "cf312b768b25dcd8d6de335762f359d891544a28";
+            string apiKey = "1befa3b66839aa5dba8668ba7d1e4c873147b4a8";
 
             var manager = new RedmineManager(host, apiKey);
 
-            var parameters = new NameValueCollection { { "spent_on", date }, { "limit", "1000" }, { "offset", "0" } };
-            var timeEntries = manager.GetObjectList<TimeEntry>(parameters).Where(e => e.User.Name.Contains(employeeName)).OrderBy(e => e.CreatedOn);
+            // Определим ID пользователя
+            var parameters = new NameValueCollection { { "limit", "1000" }
+                                                      ,{ "offset", "0" }
+                                                    };
+            var users = manager.GetObjectList<User>(parameters).Where(e => e.LastName.Contains(employeeName)
+                                                                       && !e.FirstName.Contains("External")
+                                                                        ).OrderBy(e => e.Id);
+            int userID = 0;
+            foreach (var u in users)
+            {
+                userID = u.Id;
+            }
 
+            // Time Entries
+
+            parameters = new NameValueCollection { { "spent_on", date }
+                                                  ,{ "limit", "1000" }
+                                                  ,{ "offset", "0" }
+                                                  ,{ "user_id", userID.ToString() }
+                                                 };
+            DateTime d = DateTime.Now;
+            DateTime.TryParse(date, out d);
+
+            var timeEntries = manager.GetObjectList<TimeEntry>(parameters).Where(e => e.SpentOn.Equals(d))
+                                                .OrderBy(e => e.CreatedOn);
+
+            int count = timeEntries.Count();
 
             int i = 0;
 
@@ -42,45 +65,84 @@ namespace RedmineTimeConsumer
 
             bool isWrittenEmployeeName = false;
 
+            //var path = Path.GetTempFileName() + ".txt";
+            var pathTime = string.Format("{0}-time.txt", date);
+            var pathReport = string.Format("{0}-report.txt", date);
+            StringBuilder sbOut = new StringBuilder();
+            double hoursSum = 0;
+            string name = string.Empty;
 
-            var path = Path.GetTempFileName() + ".txt";
-            using (var sw = new StreamWriter(path))
+            using (var swReport = new StreamWriter(pathReport, true))
             {
-                sw.WriteLine("--------------");
-
-                foreach (var timeEntry in timeEntries) //
+                using (var swTime = new StreamWriter(pathTime, true))
                 {
-                    if (!isWrittenEmployeeName)
+                    //sw.WriteLine("--------------");
+                    sbOut.AppendLine("--------------");
+
+                    foreach (var timeEntry in timeEntries) //
                     {
-                        sw.WriteLine(timeEntry.User.Name);
-                        isWrittenEmployeeName = true;
+
+                        if (!isWrittenEmployeeName)
+                        {
+                            //sw.Write(timeEntry.User.Name);
+                            name = timeEntry.User.Name;
+                            sbOut.AppendLine(name);
+                            isWrittenEmployeeName = true;
+                        }
+                        strBuilder.Clear();
+                        strBuilder.AppendFormat("{0}. {1} ({2}): ", ++i, timeEntry.Project.Name, timeEntry.Activity.Name, timeEntry.SpentOn);
+                        //Issue issue=null;
+                        if (timeEntry.Issue != null)
+                        {
+                            var issue = manager.GetObject<Issue>(timeEntry.Issue.Id.ToString(), new NameValueCollection());
+                            strBuilder.AppendFormat("{0} (RM#{1})", issue.Subject, issue.Id);
+                            if (!string.IsNullOrEmpty(timeEntry.Comments))
+                                strBuilder.Append(" - ");
+                        }
+
+                        strBuilder.AppendFormat("{0} ({1} ч.);", timeEntry.Comments, timeEntry.Hours);
+                        hoursSum += (double)timeEntry.Hours;
+                        //sw.WriteLine(strBuilder.ToString());
+                        sbOut.AppendLine(strBuilder.ToString());
+
                     }
-                    strBuilder.Clear();
-                    strBuilder.AppendFormat("{0}. {1} ({2}): ", ++i, timeEntry.Project.Name, timeEntry.Activity.Name);
-                    //Issue issue=null;
-                    if (timeEntry.Issue != null)
+
+                    string overTime = string.Empty;
+                    if ((8 - hoursSum) > 0)
                     {
-                        var issue = manager.GetObject<Issue>(timeEntry.Issue.Id.ToString(), new NameValueCollection());
-                        strBuilder.AppendFormat("{0} (RM#{1})", issue.Subject, issue.Id);
-                        if (!string.IsNullOrEmpty(timeEntry.Comments))
-                            strBuilder.Append(" - ");
+                        overTime = string.Format("[-{0:0.00}]", 8 - hoursSum);
                     }
-                    
-                    strBuilder.AppendFormat("{0} ({1} ч.);", timeEntry.Comments, timeEntry.Hours);
-                    sw.WriteLine(strBuilder.ToString());
+                    else if ((8 - hoursSum) < 0)
+                    {
+                        overTime = string.Format("[+{0:0.00}]", hoursSum - 8);
+                    }
+                    else
+                    {
+                        overTime = string.Empty;
+                    }
+
+                    if (!string.IsNullOrEmpty(name))
+                    {
+                        swTime.WriteLine("{0,15} [{1:0.00}] {2}", employeeName, hoursSum, overTime);
+                    }
+                    else
+                    {
+                        swTime.WriteLine("{0,15} [----]", employeeName);
+
+                    }
+                    //System.Console.WriteLine(sbOut.ToString());
+                    swReport.WriteLine(sbOut.ToString());
 
                 }
-
             }
+            //var p = Process.Start("notepad.exe", path);
 
-            var p = Process.Start("notepad.exe", path);
+            //if (p != null)
+            //{
+            //    p.WaitForExit();
+            //}
 
-            if (p != null)
-            {
-                p.WaitForExit();
-            }
-
-            File.Delete(path);
+            //File.Delete(path);
 
             //Create a issue.
             //var newIssue = new Issue { Subject = "test", Project = new IdentifiableName { Id = 1 } };
